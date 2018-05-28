5c08936702b475795619def144d8eebc6c2ed484
karthikraobr <karthik.0703@gmail.com>
2018-05-28 03:38:21 +0200

    Docs and benches

--- server.go
+++ server.go
@@ -13,19 +13,15 @@ import (
 )
 
 const (
-	endpoint          = "/numbers"
-	maxConnections    = 200
-	individualTimeout = 500
+	endpoint = "/numbers"
+	// The below 3 values should reside as environment variables for flexibility
+	// Max number of simultaneous workers
+	maxConnections = 200
+	// Timeout for requests. This is high in case the result of each URL contains millions of digits
+	individualTimeout = 50000
+	// Timeout for context. This is high in case we have to process large number of URLs
+	timeout = 50000
 )
-
-// func foo() []string {
-// 	query := []string{"http://127.0.0.1:8090/fibo", "http://127.0.0.1:8090/rand", "http://127.0.0.1:8090/odd", "http://127.0.0.1:8090/primes"}
-// 	var res []string
-// 	for i := 0; i < 10000000; i++ {
-// 		res = append(res, query[rand.Intn(len(query))])
-// 	}
-// 	return res
-// }
 
 //Type which represents the response of the given URLs as well as our response
 type result struct {
@@ -37,13 +33,8 @@ type payload struct {
 	err chan error
 }
 
-var (
-	timeout *time.Duration
-)
-
 func main() {
 	listenAddr := flag.String("http.addr", ":8000", "http listen address")
-	timeout = flag.Duration("timeout", 50000, "the total timeout for all the request")
 	flag.Parse()
 	http.HandleFunc(endpoint, numbersHandler)
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
@@ -56,44 +47,53 @@ func numbersHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	ctx := r.Context()
-	ctx, cancel := context.WithTimeout(ctx, *timeout*time.Millisecond)
+	ctx, cancel := context.WithTimeout(ctx, timeout*time.Millisecond)
 	defer cancel()
 	u := r.URL
 	q := u.Query()
 	params := q["u"]
-	fmt.Printf("Length of urls is %d\n", len(params))
 	if len(params) == 0 {
 		json.NewEncoder(w).Encode(result{Numbers: []int{}})
 	} else {
+		// Create the http transport for reuse
 		t := &http.Transport{
-			Proxy:                 http.ProxyFromEnvironment,
-			MaxIdleConnsPerHost:   maxConnections,
+			Proxy:               http.ProxyFromEnvironment,
+			MaxIdleConnsPerHost: maxConnections,
+			// Timeout for individual requests
 			ResponseHeaderTimeout: individualTimeout * time.Millisecond,
 		}
 		res := make(chan result, maxConnections)
 		err := make(chan error, maxConnections)
 		p := payload{res: res, err: err}
+		// Spawn go routines for worker to consume
 		go fetchAll(ctx, t, params, &p)
+		// Consumer to consume from channels
 		json.NewEncoder(w).Encode(result{Numbers: consume(ctx, len(params), &p)})
 	}
 }
 
+// Spawns worker goroutines and generate work
 func fetchAll(ctx context.Context, t *http.Transport, urls []string, p *payload) {
 	c := make(chan string)
-	// Spin up workers
+	// Spin up workers. Only 200 workers will be concurrently fetching from URLs.
+	// This will ensure we do not run out of sockets or hit file descriptor limits
 	for i := 0; i < maxConnections; i++ {
 		go doWork(ctx, t, c, p)
 	}
+	// Queue up work by putting URLs in a queue. The doWork goroutine will consume this channel.
 	for _, u := range urls {
 		c <- u
 	}
+	// Closing channel to indicate to doWork that we have processed all URLs
 	close(c)
 }
 
 func doWork(ctx context.Context, t *http.Transport, u chan string, p *payload) {
+	// Consume URLs until the channel is closed
 	for {
-		url := <-u
-		if url == "" {
+		url, ok := <-u
+		//Channel is closed signaling all URLs have been processed
+		if !ok {
 			return
 		}
 		fetch(ctx, t, url, p)
@@ -113,6 +113,7 @@ func fetch(ctx context.Context, t *http.Transport, u string, p *payload) {
 		p.err <- fmt.Errorf("%s returned an error while performing a request  - %v", u, err)
 		return
 	}
+	// Close body so that sockets can be reused.
 	defer res.Body.Close()
 	if res.StatusCode != http.StatusOK {
 		p.err <- fmt.Errorf("%s server returned an error - %v", u, res.Status)
@@ -122,10 +123,11 @@ func fetch(ctx context.Context, t *http.Transport, u string, p *payload) {
 		p.err <- fmt.Errorf("%s decoding error - %v", u, err)
 		return
 	}
-	log.Println("success")
+	//log.Println("success")
 	p.res <- number
 }
 
+// Consumer to drain result and error channel. Also handles context timeouts.
 func consume(ctx context.Context, count int, p *payload) []int {
 	accumulator := make([]int, 0)
 	visited := make(map[int]struct{})
--- server_test.go
+++ server_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -148,4 +150,50 @@ func (a *result) equals(b result) bool {
 		}
 	}
 	return true
+}
+
+func BenchmarkNumbersHandler(b *testing.B) {
+	ts := httptest.NewServer(http.HandlerFunc(simpleHandler([]int{1, 1, 2, 3, 5, 8, 13, 21})))
+	var buf bytes.Buffer
+	for i := 0; i < 100000; i++ {
+		buf.WriteString("&u=")
+		buf.WriteString(ts.URL)
+	}
+	req, _ := http.NewRequest(http.MethodGet, localhost+"?u="+ts.URL+buf.String(), nil)
+	tsLoad := httptest.NewServer(http.HandlerFunc(loadHandler()))
+
+	reqLoad, _ := http.NewRequest(http.MethodGet, localhost+"?u="+tsLoad.URL+"&u="+tsLoad.URL+"&u="+tsLoad.URL+"&u="+tsLoad.URL+"&u="+tsLoad.URL+"&u="+tsLoad.URL+"&u="+tsLoad.URL+"&u="+tsLoad.URL+"&u="+tsLoad.URL+"&u="+tsLoad.URL, nil)
+	tt := []struct {
+		name string
+		req  *http.Request
+	}{
+		{name: "tonsOfUrls", req: req},
+		{name: "tonsOfNumbers", req: reqLoad},
+	}
+
+	for _, tc := range tt {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				rec := httptest.NewRecorder()
+				numbersHandler(rec, tc.req)
+				res := rec.Result()
+				defer res.Body.Close()
+			}
+		})
+	}
+}
+
+func loadHandler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(map[string]interface{}{"numbers": foo()})
+	}
+}
+
+func foo() []int {
+	hold := make([]int, 1000000)
+	for i := 0; i < 1000000; i++ {
+		a := rand.Intn(100000000)
+		hold = append(hold, a)
+	}
+	return hold
 }
